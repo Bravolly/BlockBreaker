374bbfa0527633c84e562987de074bee812e7772
Bravolly <bravolly@gmail.com>
2018-08-28 17:57:45 -0700

    *Limit paddle to edge of screen *Set ball to paddle *Launch ball on click *Add walls to game *Set gravity to 0 *Destroy block on collision

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour {
-    [SerializeField] float screenWidthInUnits = 16;
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float mouseXMin = 1f;
+    [SerializeField] float mouseXMax = 15f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float mousePosX = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 paddlePos = new Vector2(mousePosX, transform.position.y);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosX, mouseXMin, mouseXMax);
         transform.position = paddlePos;
 	}
 }
