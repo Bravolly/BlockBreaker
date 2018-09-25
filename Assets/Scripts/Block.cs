fa0571df302af48e4ba358488a9f51157c0f7d9f
Bravolly <bravolly@gmail.com>
2018-09-24 22:38:16 -0700

    created level 2 add audio implement counting of blocks add win state add game speed

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,12 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour {
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    [SerializeField] AudioClip breakSound;
+
+    // chached references
+    Level level;
+
+    private void Start()
     {
-        Destroy(gameObject);
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        Destroy(gameObject);
+        level.SubtractBreakableBlock();
+    }
 }
