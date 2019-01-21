16ccbbaf4c02784ec7a565f7da8c7fbb7ecd4f12
Bravolly <bravolly@gmail.com>
2019-01-20 20:52:36 -0800

    Implement dev autoplay, add levels

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -51,8 +51,8 @@ public class Ball : MonoBehaviour {
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Vector2 velocityTweak = new Vector2
-            (Random.Range(0f, randomFactor),
-            Random.Range(0f, randomFactor));
+            (Random.Range(-1*randomFactor, randomFactor),
+            Random.Range(-1*randomFactor, randomFactor));
 
         if (hasStarted)
         {
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -7,16 +7,32 @@ public class Paddle : MonoBehaviour {
     [SerializeField] float mouseXMin = 1f;
     [SerializeField] float mouseXMax = 15f;
 
+    // cached references
+    GameSession theGameSession;
+    Ball theBall;
+
     // Use this for initialization
     void Start () {
-		
+        theGameSession = FindObjectOfType<GameSession>();
+        theBall = FindObjectOfType<Ball>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float mousePosX = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
-        paddlePos.x = Mathf.Clamp(mousePosX, mouseXMin, mouseXMax);
+        paddlePos.x = Mathf.Clamp(GetXPos(), mouseXMin, mouseXMax);
         transform.position = paddlePos;
 	}
+
+    private float GetXPos()
+    {
+        if(theGameSession.IsAutoPlayEnabled())
+        {
+            return theBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
+    }
 }
